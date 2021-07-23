74230a86ab4b3e848cf99978c35273982be2ac08
đức khiêm <66940340+maiduckhiem@users.noreply.github.com>
2021-07-23 09:13:13 +0700

    duc khiem

--- src/lesson1/indexs.ts
+++ src/lesson1/indexs.ts
@@ -4,18 +4,32 @@ interface IComponent {
   afterRender: () => void;
 }
 
-class Compoment implements IComponent{
-  public async render(){
+abstract class Component implements IComponent{
+  public  render(){
 
     //thêm dấu ! sau querySelector
     //=> thông báo với typescript : chắc chắn có một phần tử trong dom có id là root 
-    //& document.queryselector('root') không 
+    //& document.queryselector('root') không bao giờ null
       document.querySelector("#root")!.innerHTML = await this .template();
 
     //hiển thị giao diện
-    await this.afterRender();
+    // await this.afterRender();
 
   }
   public abstract  afterRender ():void;
   public abstract template (): string;
 }
+
+class UserIndex extends Component {
+    public template(){
+      return`
+        <h1>Hello</h1>
+      `
+    }
+    public afterRender(){
+      console.log('UserIndex@afterRender');
+    }
+}
+
+let gui: UserIndex= new UserIndex();
+gui.render();
